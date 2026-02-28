29caa561a19ad9e6f5c21fdd20d1afd8fe7008cd
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-02-28 18:38:38 -0500

    tiddly: add depends_on

--- Casks/t/tiddly.rb
+++ Casks/t/tiddly.rb
@@ -12,6 +12,8 @@ cask "tiddly" do
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
+  depends_on macos: ">= :monterey"
+
   app "TiddlyDesktop-mac#{arch}-v#{version}/TiddlyDesktop.app"
 
   zap trash: [
